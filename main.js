9e333784e1ba113089eabd009dd14e47458ef1c5
Josathan <jonathan.cardozo.dev@gmail.com>
2022-01-12 12:51:59 -0500

    Applying inheritance(herencia) in JS

--- main.js
+++ main.js
@@ -11,7 +11,8 @@ function videoStop(id) {
 /*
 Con export son para definir cuales son las partes que podremos ejecutar desde cualquier otro script en nuestro codigo
 */
-export class PlatziClass {
+// Quitaremos la palabra reservada expert pues hemos eliminado la compatibilidad con modulos
+class PlatziClass {
   constructor({ name, videoID }) {
     this.name = name;
     this.videoID = videoID;
@@ -28,12 +29,23 @@ export class PlatziClass {
 
 // Definicion de clase course
 class Course {
-  constructor({ name, classes = [], commets = [] }) {
+  constructor({
+    name,
+    classes = [],
+    commets = [],
+    /*se agregan  estos parametros para la comprobacion*/
+    isFree = false,
+    lang = "spanish",
+  }) {
     // nomenclatura de _guionBajo al incio se usa para hacer saber al resto que no llamen o modifiquen estos metodos o atributos desde afuera ya que son "privados" o eso es lo que lograremos haciendo:
     // Para eso escondemos el atributo _name dentro del prototipo Course
     //Luego solo tendran que entrar por el get o el set
     this._name = name;
     this.classes = classes;
+    this.commets = commets;
+    // Tambien lo hacemos aqui
+    this.isFree = isFree;
+    this.lang = lang;
   }
 
   // Para conseguir el valor de los atributos haremos lo siguiente:
@@ -55,9 +67,15 @@ class Course {
 
 const cursoProgBasica = new Course({
   name: "Curso Gratis de Programacion Basica",
+  // El valor por defecto isFree es false aqui cambiamos su valor
+  isFree: true,
 });
 const cursoDefinitivoHTML = new Course({
   name: "Curso Definitivo de HTML & CSS",
+});
+const cursoPracticoHTML = new Course({
+  name: "Curso Practico de HTML & CSS",
+  lang: "english",
 });
 
 // Definicion de LearningPath
@@ -84,7 +102,7 @@ const escuelaVgs = new LearningPath({
 });
 
 // Defincion de Student
-class Student {
+class StudentMom {
   constructor({
     name,
     email,
@@ -110,16 +128,74 @@ class Student {
   }
 }
 
-const jose = new Student({
+// Aplicando herencia de clase StudentMama
+// Para que las subclases puedan heredad usamos extends claseMadre
+class FreeStudent extends StudentMom {
+  // Para hereda el constructor de clase StudentMom
+  constructor(props) {
+    // usamos la palabra reservada super(props) que pasara todos estos agumentos en props al constructor
+    super(props);
+  }
+
+  approveCourse(newCourse) {
+    if (newCourse.isFree) {
+      this.approvedCourses.push(newCourse);
+    } else {
+      console.warn(
+        "Lo sentimos, " + this.name + ", solo puedes tomar cursos abiertos",
+      );
+    }
+    if (newCourse.lang !== "english") {
+      this.approvedCourses.push(newCourse);
+    } else {
+      console.warn(
+        "Lo sentimos, " + this.name + ", no puedes tomar cursos en inglés",
+      );
+    }
+  }
+}
+
+class BasicStudent extends StudentMom {
+  constructor(props) {
+    super(props);
+  }
+
+  approveCourse(newCourse) {
+    if (newCourse.lang !== "english") {
+      this.approvedCourses.push(newCourse);
+    } else {
+      console.warn(
+        "Lo sentimos, " + this.name + ", no puedes tomar cursos en inglés",
+      );
+    }
+  }
+}
+
+class ExpertStudent extends StudentMom {
+  constructor(props) {
+    super(props);
+  }
+  // Para usuarios Expert no hay ningun tipo de comprobacion
+  approveCourse(newCourse) {
+    this.approvedCourses.push(newCourse);
+  }
+}
+
+const jose = new BasicStudent({
   name: "jose",
   username: "arturo",
   email: "artur@mail.com",
   twiter: "artuiter",
 });
 
-const jose1 = new Student({
+const arturo = new FreeStudent({
   name: "jose",
   username: "arturo",
   email: "arturito@mail.com",
   instagram: "artuiter",
 });
+
+console.log(arturo.approveCourse(cursoDefinitivoHTML));
+console.log(arturo.approveCourse(cursoPracticoHTML));
+console.log(jose.approveCourse(cursoDefinitivoHTML));
+console.log(jose.approvedCourses[0]);
