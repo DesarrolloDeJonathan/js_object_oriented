9355443df3e1257a2c2648419beb63c3f6d3ee05
Josathan <jonathan.cardozo.dev@gmail.com>
2022-01-16 23:49:54 -0500

    Module pattern and namespaces: private properties in JavaScript no comments

--- intermedium_oop/main.js
+++ intermedium_oop/main.js
@@ -37,13 +37,10 @@ function deepCopy(subject) {
   return copySubject;
 }
 
-// **RORO ** => Recibir un Objecto, y Retornamos otro Objeto
-// Esta funcion la creamos para establecer un parametro como obligatorio
 function requiredParam(param) {
-  throw new Error(`${param}es obligatorio`);
+  throw new Error(param + " es obligatorio");
 }
 
-// Fabirica de objetos
 function createStudent({
   name = requiredParam("name"),
   email = requiredParam("email"),
@@ -54,16 +51,10 @@ function createStudent({
   approvedCourses = [],
   learningPaths = [],
 } = {}) {
-  /* Dividiremos el codigo en partes declarando esta variable objeto dentro de la funcion createStudent
-     Contendra las propiedades que no queremos que se cambien directamente
-     Para hacerlo tendran que usar los metodos dentro de public
-    */
   const private = {
-    /* variable con convencion de guion bajo indicando que es privada */
     _name: name,
   };
 
-  /* Las propiedades que podran ser trabajadas sin restriciones, sin ninguna validacion */
   const public = {
     email,
     age,
@@ -74,35 +65,27 @@ function createStudent({
       instagram,
       facebook,
     },
-    /* La usamos para actualizar el nombre y hacer una validacion/es a name
-    // Tambien podriamos hacer una validacion para comprobar que el email no se repite
-    // Para que solo se pueda cambiar la propiedad name por medio de esta funcion implementaremos otras soluciones
-    */
-    changeName(newName) {
-      private["_name"] = newName;
-    },
-    /* Necesitaremos tambien como ver el nombre definido como "privado"*/
     readName() {
       return private["_name"];
     },
+    changeName(newName) {
+      private["_name"] = newName;
+    },
   };
 
-  // Esto lo hacemos para proteger nuestra propiedad metodo changeName()
-  // La desventaja es que no podremos aplicar polimorfismo cuando aplicamos esta proteccion
-  // Ya que no podemos editarla, pero en este caso no queremos que estas funciones sean alteradas
-  // Los demas atributos no seran afectados con esta proteccion
-  Object.defineProperty(public, "changeName", {
-    writable: false,
-    configurable: false,
-  });
   Object.defineProperty(public, "readName", {
     writable: false,
     configurable: false,
   });
+  Object.defineProperty(public, "changeName", {
+    writable: false,
+    configurable: false,
+  });
 
-  return {};
+  return public;
 }
 
+const juan = createStudent({ email: "juanito@frijoles.co", name: "Juanito" });
 const jonny = createStudent({
   email: "yonoc@cualmail.co",
   name: "Jonny C",
