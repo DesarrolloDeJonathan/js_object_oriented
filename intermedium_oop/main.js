e90ab246d8db7c3b52b5445a93499eb7767ab8ad
Josathan <jonathan.cardozo.dev@gmail.com>
2022-01-16 23:46:15 -0500

    Module pattern and namespaces: private properties in JavaScript With Comments

--- intermedium_oop/main.js
+++ intermedium_oop/main.js
@@ -37,51 +37,34 @@ function deepCopy(subject) {
   return copySubject;
 }
 
-// Objeto literal
-// const studentBase = {
-//   name: undefined,
-//   email: undefined,
-//   age: undefined,
-//   approvedCourses: undefined,
-//   learningPaths: undefined,
-//   socialMedia: {
-//     twitter: undefined,
-//     instagram: undefined,
-//     facebook: undefined,
-//   },
-// };
-
 // **RORO ** => Recibir un Objecto, y Retornamos otro Objeto
 // Esta funcion la creamos para establecer un parametro como obligatorio
 function requiredParam(param) {
-  // Lanzara un nuevo objeto Error con el mensaje y el nombre que esta recibiendo como parametro:
-  // el nombre de la llave name del objeto que recibe por parametro la funcion createStudent({})
   throw new Error(`${param}es obligatorio`);
 }
 
 // Fabirica de objetos
-// Levara solo un parametro y sera un objeto que dentro tendra las propiedades que esperamos recibir
-// No importa el orden de llegada o de salida, las propiedades faltantes seran undefinded por defecto
-// a menos que sea requerida como obligatoria esa propiedad
 function createStudent({
-  // Al llamar a la funcion createStudent() sin este valor mostrara error e indicara que es obligatorio
-  // Por defecto tendra el valor de requerido y lanzara error a no ser que envie ese valor y sobrescribiendo el valor por defecto
   name = requiredParam("name"),
   email = requiredParam("email"),
   age,
   twitter,
   instagram,
   facebook,
-  // Parametro por defecto
   approvedCourses = [],
   learningPaths = [],
-  // Por defecto este parametro tendra el varlor de un objeto vacio
-  // Y en caso que no se envie nada no tendra error y continuara la ejecucion
-  //
 } = {}) {
-  return {
-    // Retornaresmos de esta manera ya que no alteraremos sus valores
-    name,
+  /* Dividiremos el codigo en partes declarando esta variable objeto dentro de la funcion createStudent
+     Contendra las propiedades que no queremos que se cambien directamente
+     Para hacerlo tendran que usar los metodos dentro de public
+    */
+  const private = {
+    /* variable con convencion de guion bajo indicando que es privada */
+    _name: name,
+  };
+
+  /* Las propiedades que podran ser trabajadas sin restriciones, sin ninguna validacion */
+  const public = {
     email,
     age,
     approvedCourses,
@@ -91,19 +74,36 @@ function createStudent({
       instagram,
       facebook,
     },
+    /* La usamos para actualizar el nombre y hacer una validacion/es a name
+    // Tambien podriamos hacer una validacion para comprobar que el email no se repite
+    // Para que solo se pueda cambiar la propiedad name por medio de esta funcion implementaremos otras soluciones
+    */
+    changeName(newName) {
+      private["_name"] = newName;
+    },
+    /* Necesitaremos tambien como ver el nombre definido como "privado"*/
+    readName() {
+      return private["_name"];
+    },
   };
-}
 
-// Este objeto se cargara con todo undefined a menos que
-const vacio = createStudent({});
-// Si no enviamos ningun parametro u objeto vamos a tener un error
-// Pues en la funcion createStudent() asumimos que se envia un objeto con esas propiedades,
-// y al ser  por defecto indefinido = al retorno que es indefinido dara error
-// Esto lo solucionamos o lo perdonamos por asi decirlo haciendo que el parametro de la funcion por defecto sea un objeto vacio
-const vacio = createStudent();
+  // Esto lo hacemos para proteger nuestra propiedad metodo changeName()
+  // La desventaja es que no podremos aplicar polimorfismo cuando aplicamos esta proteccion
+  // Ya que no podemos editarla, pero en este caso no queremos que estas funciones sean alteradas
+  // Los demas atributos no seran afectados con esta proteccion
+  Object.defineProperty(public, "changeName", {
+    writable: false,
+    configurable: false,
+  });
+  Object.defineProperty(public, "readName", {
+    writable: false,
+    configurable: false,
+  });
+
+  return {};
+}
 
 const jonny = createStudent({
   email: "yonoc@cualmail.co",
   name: "Jonny C",
-  age: 25,
 });
