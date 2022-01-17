4245b059ab037f7ea1a478690e9fe82d3fe9010e
Josathan <jonathan.cardozo.dev@gmail.com>
2022-01-16 21:48:36 -0500

    Factory pattern & RORO

--- intermedium_oop/main.js
+++ intermedium_oop/main.js
@@ -38,39 +38,72 @@ function deepCopy(subject) {
 }
 
 // Objeto literal
-const studentBase = {
-  name: undefined,
-  email: undefined,
-  age: undefined,
-  approvedCourses: undefined,
-  learningPaths: undefined,
-  socialMedia: {
-    twitter: undefined,
-    instagram: undefined,
-    facebook: undefined,
-  },
-};
+// const studentBase = {
+//   name: undefined,
+//   email: undefined,
+//   age: undefined,
+//   approvedCourses: undefined,
+//   learningPaths: undefined,
+//   socialMedia: {
+//     twitter: undefined,
+//     instagram: undefined,
+//     facebook: undefined,
+//   },
+// };
 
-const jonny = deepCopy(studentBase);
-const stivi = deepCopy(studentBase);
+// **RORO ** => Recibir un Objecto, y Retornamos otro Objeto
+// Esta funcion la creamos para establecer un parametro como obligatorio
+function requiredParam(param) {
+  // Lanzara un nuevo objeto Error con el mensaje y el nombre que esta recibiendo como parametro:
+  // el nombre de la llave name del objeto que recibe por parametro la funcion createStudent({})
+  throw new Error(`${param}es obligatorio`);
+}
 
-// Object.defineProperty(jonny, "name", {
-//   value: "Juanito",
-//   // Pondremos esta en false las demas sabemos que vienen por defecto en true
-//   configurable: false,
-// });
+// Fabirica de objetos
+// Levara solo un parametro y sera un objeto que dentro tendra las propiedades que esperamos recibir
+// No importa el orden de llegada o de salida, las propiedades faltantes seran undefinded por defecto
+// a menos que sea requerida como obligatoria esa propiedad
+function createStudent({
+  // Al llamar a la funcion createStudent() sin este valor mostrara error e indicara que es obligatorio
+  // Por defecto tendra el valor de requerido y lanzara error a no ser que envie ese valor y sobrescribiendo el valor por defecto
+  name = requiredParam("name"),
+  email = requiredParam("email"),
+  age,
+  twitter,
+  instagram,
+  facebook,
+  // Parametro por defecto
+  approvedCourses = [],
+  learningPaths = [],
+  // Por defecto este parametro tendra el varlor de un objeto vacio
+  // Y en caso que no se envie nada no tendra error y continuara la ejecucion
+  //
+} = {}) {
+  return {
+    // Retornaresmos de esta manera ya que no alteraremos sus valores
+    name,
+    email,
+    age,
+    approvedCourses,
+    learningPaths,
+    socialMedia: {
+      twitter,
+      instagram,
+      facebook,
+    },
+  };
+}
 
-// Este atajo tambien evita que borremos las propiedades de nuestros objetos en menos codigo
-// Es decir con el configurable: false
-Object.seal(jonny);
-// juan.name = "Juanito";
+// Este objeto se cargara con todo undefined a menos que
+const vacio = createStudent({});
+// Si no enviamos ningun parametro u objeto vamos a tener un error
+// Pues en la funcion createStudent() asumimos que se envia un objeto con esas propiedades,
+// y al ser  por defecto indefinido = al retorno que es indefinido dara error
+// Esto lo solucionamos o lo perdonamos por asi decirlo haciendo que el parametro de la funcion por defecto sea un objeto vacio
+const vacio = createStudent();
 
-// La usamos para saber si el objeto tiene todas sus propiedades protegidas con seal
-// Es decir con el configurable: false
-console.log(Object.isSealed(jonny));
-
-// Establecemos writable: false y configurable: false
-// Ahora no se podra borrar ni sobrescribir
-Object.freeze(stivi);
-// Y con este comprobamos que las
-console.log(Object.isFrozen(stivi));
+const jonny = createStudent({
+  email: "yonoc@cualmail.co",
+  name: "Jonny C",
+  age: 25,
+});
