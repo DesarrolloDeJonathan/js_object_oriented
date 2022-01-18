040220840117169b4ee22312692de0e02e8a58cd
Josathan <jonathan.cardozo.dev@gmail.com>
2022-01-17 22:27:04 -0500

    Instance Of in JavaScript with Instances and Prototypes

--- intermedium_oop/main.js
+++ intermedium_oop/main.js
@@ -41,36 +41,37 @@ function requiredParam(param) {
   throw new Error(param + " es obligatorio");
 }
 
-// Para crear nuevas rutas de aprendizaje con estos criterios(fabricara estos objetos)
-function createLearningPath({ name = requiredParam("name"), courses = [] }) {
-  // Para interactuar con lo privado usaremos getter & setters que estan publicos
-  const private = {
-    _name: name,
-    _courses: courses,
-  };
+// El mombre cambia por que ya no es una fabrica de objetos sino un Prototipo
+function LearningPath({ name = requiredParam("name"), courses = [] }) {
+  this.name = name;
+  this.courses = courses;
 
-  const public = {
-    // Para acceder al nombre de la ruta de aprendizaje
-    get name() {
-      return private["_name"];
-    },
-    // De momento no omitiremos esta hasta definir que roles podran editar este campo
-    set name(newName) {
-      if (newName.length != 0) {
-        private["_name"] = newName;
-      } else {
-        console.warn("Tu nombre debe tener al menos 1 caracter");
-      }
-    },
-    get courses() {
-      return private["_courses"];
-    },
-  };
+  // const private = {
+  //   "_name": name,
+  //   "_courses": courses,
+  // };
 
-  return public;
+  // const public = {
+  //   get name() {
+  //     return private["_name"];
+  //   },
+  //   set name(newName) {
+  //     if (newName.length != 0) {
+  //       private["_name"] = newName;
+  //     } else {
+  //       console.warn("Tu nombre debe tener al menos 1 caracter");
+  //     }
+  //   },
+  //   get courses() {
+  //     return private["_courses"];
+  //   },
+  // };
+
+  // return public;
 }
 
-function createStudent({
+// El mombre cambia por que ya no es una fabrica de objetos sino un Prototipo
+function Student({
   name = requiredParam("name"),
   email = requiredParam("email"),
   age,
@@ -80,76 +81,100 @@ function createStudent({
   approvedCourses = [],
   learningPaths = [],
 } = {}) {
-  const private = {
-    _name: name,
-    _learningPaths: learningPaths,
+  this.name = name;
+  this.email = email;
+  this.age = age;
+  this.approvedCourses = approvedCourses;
+  this.socialMedia = {
+    twitter,
+    instagram,
+    facebook,
   };
 
-  const public = {
-    email,
-    age,
-    approvedCourses,
-    socialMedia: {
-      twitter,
-      instagram,
-      facebook,
-    },
-    get name() {
-      return private["_name"];
-    },
-    set name(newName) {
-      if (newName.length != 0) {
-        private["_name"] = newName;
-      } else {
-        console.warn("Tu nombre debe tener al menos 1 caracter");
-      }
-    },
-    // Validaciones para la propiedad privada _learningPaths
-    get learningPaths() {
-      return private["_learningPaths"];
-    },
-    set learningPaths(newLP) {
-      if (!newLP.name) {
-        console.warn("Tu LP no tiene la propiedad name");
-        return;
-      }
+  if (isArray(learningPaths)) {
+    // Si validamos que el la varaible learningPath si es un array ira cargando los learningPathIndex dentro de este array vacio
+    this.learningPaths = [];
+    // Ya validamos que es un array y sabemos que puede haber elementos en este
+    // Preguntamos si cada indice del array es una isntacia de LearningPath
+    // learningPathIndex es la pocision dentro del array o en el array learningPaths
+    for (learningPathIndex in learningPaths) {
+      console.log(learningPaths[learningPathIndex]);
+      console.log(learningPaths[learningPathIndex] instanceof LearningPath);
 
-      if (!newLP.courses) {
-        console.warn("Tu LP no tiene courses");
-        return;
+      // Si cada uno es realmente un LearningPath
+      if (learningPaths[learningPathIndex] instanceof LearningPath) {
+        console.log(learningPaths);
+        console.log(learningPaths);
+        // Empujara la nueva propiedad con la posicion de learningPathIndex dentro del array learningPath
+        this.learningPaths.push(learningPaths[learningPathIndex]);
+        console.log(learningPaths);
+        console.log(learningPaths[learningPathIndex]);
       }
+    }
+  }
 
-      if (!isArray(newLP.courses)) {
-        console.warn("Tu LP no es una lista (*de cursos)");
-        return;
-      }
+  // const private = {
+  //   "_name": name,
+  //   "_learningPaths": learningPaths,
+  // };
 
-      // Aqui en vez de hacer una asignacion usamos el metodo push para empujar newLP
-      // Y newLP no pase de ser un array a un objeto, sino que se agrege a la lista
-      private["_learningPaths"].push(newLP);
-    },
-  };
+  // const public = {
+  //   email,
+  //   age,
+  //   approvedCourses,
+  //   socialMedia: {
+  //     twitter,
+  //     instagram,
+  //     facebook,
+  //   },
+  //   get name() {
+  //     return private["_name"];
+  //   },
+  //   set name(newName) {
+  //     if (newName.length != 0) {
+  //       private["_name"] = newName;
+  //     } else {
+  //       console.warn("Tu nombre debe tener al menos 1 caracter");
+  //     }
+  //   },
+  //   get learningPaths() {
+  //     return private["_learningPaths"];
+  //   },
+  //   set learningPaths(newLP) {
+  //     if (!newLP.name) {
+  //       console.warn("Tu LP no tiene la propiedad name");
+  //       return;
+  //     }
 
-  return public;
+  //     if (!newLP.courses) {
+  //       console.warn("Tu LP no tiene courses");
+  //       return;
+  //     }
+
+  //     if (!isArray(newLP.courses)) {
+  //       console.warn("Tu LP no es una lista (*de cursos)");
+  //       return;
+  //     }
+
+  //     private["_learningPaths"].push(newLP);
+  //   },
+  // };
+
+  // return public;
 }
 
-const juan = createStudent({ email: "juanito@frijoles.co", name: "Juanito" });
-const jonny = createStudent({
-  email: "yonoc@cualmail.co",
-  name: "Jonny C",
-  age: 55,
+const escuelaWeb = new LearningPath({ name: "Escuela de WebDev" });
+const escuelaData = new LearningPath({ name: "Escuela de Data Science" });
+const juan = new Student({
+  email: "juanito@frijoles.co",
+  name: "Juanito",
+  // Con la logica aplicada en el condicional de arriaba este al no ser una instasia de LearningPath
+  // perdera relevacia y no se mostrara con lo asi antes
+  learningPaths: [escuelaWeb, escuelaData, { name: "escuela DEL IMPOSTOR" }],
 });
 
-// Esta la usamos para crear una escuela pero no se esta comprobando que learningPaths se cree en createLearningPaths
-// const escuelaData = createLearningPath({
-//   name: "Escuela de Data Science",
-//   courses: [],
-// });
-
-// al estar trabajndo con get & set podremos declararla de esta forma apesar que sean funciones
-// jonny.learningPaths = "Nueva ruta de aprendizaje"; dara erro al no ser un objeto con los datos esperados
-// Esta la usamos para crear una ruta dentro del studiante y es lo mismo ya que ha heredado
-// En este momento estamos validando que valores tienen mas no que tipo son
-// y no estamos validando si nuestro learningPaths son objetos creados en nuestra funcion createLearningPath
-// jonny.learningPaths = { name: "Escuela de desarrolo Web", courses: [] };
-// console.log(jonny.learningPaths);
+const jonny = new Student({
+  email: "yonoc@cualmail.co",
+  name: "Jonny C",
+  learningPaths: [escuelaWeb, escuelaData],
+});
